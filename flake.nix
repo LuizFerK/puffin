ba34c8089040b45eed88bb00d581e8fd7b2784bc
LuizFerK <luizfernandoklein@live.com>
2025-02-23 14:10:54 -0300

    init: Tauri + Solid + Vite setup

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay"; # Add rust-overlay for Rust version management
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ]; # Import rust-overlay
+        pkgs = import nixpkgs { inherit system overlays; }; # Apply the overlay
+
+        libraries = with pkgs;[
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+        ];
+
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
+          libsoup_3
+          webkitgtk_4_1
+          nodejs # Add nodejs
+          pnpm # Add pnpm
+          (rust-bin.stable.latest.default.override { # Use rust-bin for Rust 1.78 or higher
+            extensions = [ "rust-src" "rust-analyzer" ]; # Optional: Add rust-src and rust-analyzer
+          })
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+              $SHELL
+            '';
+        };
+      });
+}
